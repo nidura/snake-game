52f692a79015d62c9a937cda718f093b9ae116d4
nidura <niduraprageeth@gmail.com>
2021-01-06 20:15:24 +0530

    add snake and board

--- src/com/swlc/Main.java
+++ src/com/swlc/Main.java
@@ -1,4 +1,15 @@
 package com.swlc;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
 }
---
+++ src/com/swlc/Snake.java
@@ -0,0 +1,23 @@
+package com.swlc;
+
+import javax.swing.*;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
